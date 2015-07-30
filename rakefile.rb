3246659b93649549f15f27966793144007e6c2a2
Peter Davis <peter.w.davis@gmail.com>
2015-07-30 17:37:31 +0930

    Updated rake build for new nuget packages

--- rakefile.rb
+++ rakefile.rb
@@ -52,10 +52,13 @@ end
 
 desc "Cleans, versions, compiles the application and generates build_output/."
 task :compile => [:versioning, :global_version, :build] do
-	copyOutputFiles File.join(props[:src], "RapidTransit.Core/bin/Release"), "RapidTransit.Core.{dll,pdb,xml}", File.join(props[:output], 'net-4.5')
+  copyOutputFiles File.join(props[:src], "RapidTransit.Core/bin/Release"), "RapidTransit.Core.{dll,pdb,xml}", File.join(props[:output], 'net-4.5')
   copyOutputFiles File.join(props[:src], "RapidTransit.Integration/bin/Release"), "RapidTransit.Integration.{dll,pdb,xml}", File.join(props[:output], 'net-4.5')
+  copyOutputFiles File.join(props[:src], "RapidTransit.Integration.AzureServiceBus/bin/Release"), "RapidTransit.Integration.AzureServiceBus.{dll,pdb,xml}", File.join(props[:output], 'net-4.5')
+  copyOutputFiles File.join(props[:src], "RapidTransit.Integration.RabbitMQ/bin/Release"), "RapidTransit.Integration.RabbitMQ.{dll,pdb,xml}", File.join(props[:output], 'net-4.5')
   copyOutputFiles File.join(props[:src], "RapidTransit.Integration.Web/bin/Release"), "RapidTransit.Integration.Web.{dll,pdb,xml}", File.join(props[:output], 'net-4.5')
   copyOutputFiles File.join(props[:src], "RapidTransit.Integration.Services/bin/Release"), "RapidTransit.Integration.Services.{dll,pdb,xml}", File.join(props[:output], 'net-4.5')
+  copyOutputFiles File.join(props[:src], "RapidTransit.Integration.Services.Azure/bin/Release"), "RapidTransit.Integration.Services.Azure.{dll,pdb,xml}", File.join(props[:output], 'net-4.5')
 end
 
 desc "Only compiles the application."
@@ -63,7 +66,7 @@ msbuild :build do |msb|
 	msb.properties :Configuration => "Release",
 		:Platform => 'Any CPU'
 	msb.use :net4
-	msb.targets :Clean, :Build
+	msb.targets :Rebuild
   msb.properties[:SignAssembly] = 'true'
   msb.properties[:AssemblyOriginatorKeyFile] = props[:keyfile]
 	msb.solution = 'src/RapidTransit.sln'
@@ -79,7 +82,8 @@ end
 desc "Runs unit tests"
 nunit :tests => [:compile] do |nunit|
           nunit.command = File.join('src', 'packages','NUnit.Runners.2.6.3', 'tools', 'nunit-console.exe')
-          nunit.options = "/framework=#{CLR_TOOLS_VERSION}", '/nothread', '/nologo', '/labels', "\"/xml=#{File.join(props[:artifacts], 'nunit-test-results.xml')}\""
+		  nunit.no_logo
+          nunit.parameters = ["/framework=#{CLR_TOOLS_VERSION}", '/nothread', '/labels', "\"/xml=#{File.join(props[:artifacts], 'nunit-test-results.xml')}\""]
           nunit.assemblies = FileList[File.join(props[:src], "RapidTransit.Tests/bin/Release", "RapidTransit.Tests.dll")]
 end
 
@@ -87,9 +91,8 @@ task :package => [:nuget, :zip_output]
 
 desc "ZIPs up the build results."
 zip :zip_output => [:versioning] do |zip|
-	zip.directories_to_zip = [props[:output]]
-	zip.output_file = "RapidTransit-#{NUGET_VERSION}.zip"
-	zip.output_path = props[:artifacts]
+	zip.dirs = [props[:output]]
+	zip.output_path = File.join(props[:artifacts], "RapidTransit-#{NUGET_VERSION}.zip")
 end
 
 desc "Restore missing NuGet packages"
@@ -99,10 +102,13 @@ end
 
 desc "Builds the nuget package"
 task :nuget => [:versioning, :create_nuspec] do
-	sh "src/.nuget/nuget.exe pack #{props[:artifacts]}/RapidTransit.Core.nuspec /Symbols /OutputDirectory #{props[:artifacts]}"
+  sh "src/.nuget/nuget.exe pack #{props[:artifacts]}/RapidTransit.Core.nuspec /Symbols /OutputDirectory #{props[:artifacts]}"
   sh "src/.nuget/nuget.exe pack #{props[:artifacts]}/RapidTransit.Integration.nuspec /Symbols /OutputDirectory #{props[:artifacts]}"
+  sh "src/.nuget/nuget.exe pack #{props[:artifacts]}/RapidTransit.Integration.AzureServiceBus.nuspec /Symbols /OutputDirectory #{props[:artifacts]}"
+  sh "src/.nuget/nuget.exe pack #{props[:artifacts]}/RapidTransit.Integration.RabbitMQ.nuspec /Symbols /OutputDirectory #{props[:artifacts]}"
   sh "src/.nuget/nuget.exe pack #{props[:artifacts]}/RapidTransit.Integration.Web.nuspec /Symbols /OutputDirectory #{props[:artifacts]}"
-	sh "src/.nuget/nuget.exe pack #{props[:artifacts]}/RapidTransit.Integration.Services.nuspec /Symbols /OutputDirectory #{props[:artifacts]}"
+  sh "src/.nuget/nuget.exe pack #{props[:artifacts]}/RapidTransit.Integration.Services.nuspec /Symbols /OutputDirectory #{props[:artifacts]}"
+  sh "src/.nuget/nuget.exe pack #{props[:artifacts]}/RapidTransit.Integration.Services.Azure.nuspec /Symbols /OutputDirectory #{props[:artifacts]}"
 end
 
 task :create_nuspec => [:_nuspec]
@@ -110,21 +116,19 @@ task :create_nuspec => [:_nuspec]
 nuspec :_nuspec do |nuspec|
   nuspec.id = 'RapidTransit.Core'
   nuspec.version = NUGET_VERSION
-  nuspec.authors = 'Chris Patterson'
+  nuspec.authors = ["Chris Patterson"]
   nuspec.description = 'RapidTransit, a quick library to get started building autonomous services using MassTransit'
   nuspec.title = 'RapidTransit.Core'
-  nuspec.projectUrl = 'http://github.com/MassTransit/RapidTransit'
+  nuspec.project_url = 'http://github.com/MassTransit/RapidTransit'
   nuspec.language = "en-US"
-  nuspec.licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0"
-  nuspec.requireLicenseAcceptance = "false"
-  nuspec.dependency 'Automatonymous', "1.2.5"
-  nuspec.dependency 'Automatonymous.MassTransit', "1.2.5"
-  nuspec.dependency 'Magnum', "2.1.2"
-  nuspec.dependency 'MassTransit', "2.9.5"
-  nuspec.dependency 'MassTransit.Courier', "2.9.5"
-  nuspec.dependency 'MassTransit.Scheduling', "1.2.5"
-  nuspec.dependency 'Newtonsoft.Json', "5.0.8"
-  nuspec.dependency 'RabbitMQ.Client', "3.2.1"
+  nuspec.license_url = "http://www.apache.org/licenses/LICENSE-2.0"
+  nuspec.dependency 'Automatonymous', "1.2.9"
+  nuspec.dependency 'Automatonymous.MassTransit', "1.2.9"
+  nuspec.dependency 'Magnum', "2.1.3"
+  nuspec.dependency 'MassTransit', "2.10.0"
+  nuspec.dependency 'MassTransit.Courier', "2.10.0"
+  nuspec.dependency 'MassTransit.Scheduling', "1.2.10"
+  nuspec.dependency 'Newtonsoft.Json', "6.0.8"
   nuspec.dependency 'Taskell', "0.1.2"
   nuspec.output_file = File.join(props[:artifacts], 'RapidTransit.Core.nuspec')
   add_files props[:output], 'RapidTransit.Core.{dll,pdb,xml}', nuspec
@@ -134,25 +138,23 @@ end
 nuspec :_nuspec do |nuspec|
   nuspec.id = 'RapidTransit.Integration'
   nuspec.version = NUGET_VERSION
-  nuspec.authors = 'Chris Patterson'
+  nuspec.authors = ["Chris Patterson"]
   nuspec.description = 'RapidTransit, a quick library to get started building autonomous services using MassTransit'
   nuspec.title = 'RapidTransit.Integration'
-  nuspec.projectUrl = 'http://github.com/MassTransit/RapidTransit'
+  nuspec.project_url = 'http://github.com/MassTransit/RapidTransit'
   nuspec.language = "en-US"
-  nuspec.licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0"
-  nuspec.requireLicenseAcceptance = "false"
+  nuspec.license_url = "http://www.apache.org/licenses/LICENSE-2.0"
   nuspec.dependency "RapidTransit.Core", NUGET_VERSION
-  nuspec.dependency 'Autofac', "3.3.0"
-  nuspec.dependency 'Automatonymous', "1.2.5"
-  nuspec.dependency 'Automatonymous.MassTransit', "1.2.5"
-  nuspec.dependency 'Magnum', "2.1.2"
-  nuspec.dependency 'MassTransit', "2.9.5"
-  nuspec.dependency 'MassTransit.Autofac', "2.9.5"
-  nuspec.dependency 'MassTransit.Courier', "2.9.5"
-  nuspec.dependency 'MassTransit.RabbitMQ', "2.9.5"
-  nuspec.dependency 'MassTransit.Scheduling', "1.2.5"
-  nuspec.dependency 'Newtonsoft.Json', "5.0.8"
-  nuspec.dependency 'RabbitMQ.Client', "3.2.1"
+  nuspec.dependency 'Autofac', "3.5.2"
+  nuspec.dependency 'Automatonymous', "1.2.9"
+  nuspec.dependency 'Automatonymous.MassTransit', "1.2.9"
+  nuspec.dependency 'Magnum', "2.1.3"
+  nuspec.dependency 'MassTransit', "2.10.0"
+  nuspec.dependency 'MassTransit.Autofac', "2.10.0"
+  nuspec.dependency 'MassTransit.Courier', "2.10.0"
+  nuspec.dependency 'MassTransit.RabbitMQ', "2.10.0"
+  nuspec.dependency 'MassTransit.Scheduling', "1.2.10"
+  nuspec.dependency 'Newtonsoft.Json', "6.0.8"
   nuspec.dependency 'Taskell', "0.1.2"
   nuspec.output_file = File.join(props[:artifacts], 'RapidTransit.Integration.nuspec')
   add_files props[:output], 'RapidTransit.Integration.{dll,pdb,xml}', nuspec
@@ -160,28 +162,69 @@ nuspec :_nuspec do |nuspec|
 end
 
 nuspec :_nuspec do |nuspec|
+  nuspec.id = 'RapidTransit.Integration.AzureServiceBus'
+  nuspec.version = NUGET_VERSION
+  nuspec.authors = ["Chris Patterson", "Peter Davis"]
+  nuspec.description = 'RapidTransit, a quick library to get started building autonomous services using MassTransit'
+  nuspec.title = 'RapidTransit.Integration.AzureServiceBus'
+  nuspec.project_url = 'http://github.com/MassTransit/RapidTransit'
+  nuspec.language = "en-US"
+  nuspec.license_url = "http://www.apache.org/licenses/LICENSE-2.0"
+  nuspec.dependency "RapidTransit.Core", NUGET_VERSION
+  nuspec.dependency 'Autofac', "3.5.2"
+  nuspec.dependency 'Magnum', "2.1.3"
+  nuspec.dependency 'MassTransit', "2.10.0"
+  nuspec.dependency 'MassTransit.AzureServiceBus', "0.4.1"
+  nuspec.dependency 'MassTransit.Courier', "2.10.0"
+  nuspec.dependency 'Microsoft.WindowsAzure.ConfigurationManager', "2.0.3"
+  nuspec.dependency 'Newtonsoft.Json', "6.0.8"
+  nuspec.dependency 'WindowsAzure.ServiceBus', "2.6.2"
+  nuspec.output_file = File.join(props[:artifacts], 'RapidTransit.Integration.AzureServiceBus.nuspec')
+  add_files props[:output], 'RapidTransit.Integration.AzureServiceBus.{dll,pdb,xml}', nuspec
+  nuspec.file(File.join(props[:src], "RapidTransit.Integration.AzureServiceBus\\**\\*.cs").gsub("/","\\"), "src")
+end
+
+nuspec :_nuspec do |nuspec|
+  nuspec.id = 'RapidTransit.Integration.RabbitMQ'
+  nuspec.version = NUGET_VERSION
+  nuspec.authors = ["Chris Patterson", "Peter Davis"]
+  nuspec.description = 'RapidTransit, a quick library to get started building autonomous services using MassTransit'
+  nuspec.title = 'RapidTransit.Integration.RabbitMQ'
+  nuspec.project_url = 'http://github.com/MassTransit/RapidTransit'
+  nuspec.language = "en-US"
+  nuspec.license_url = "http://www.apache.org/licenses/LICENSE-2.0"
+  nuspec.dependency "RapidTransit.Core", NUGET_VERSION
+  nuspec.dependency 'Autofac', "3.5.2"
+  nuspec.dependency 'Magnum', "2.1.3"
+  nuspec.dependency 'MassTransit', "2.10.0"
+  nuspec.dependency 'MassTransit.RabbitMQ', "2.10.0"
+  nuspec.dependency 'MassTransit.Courier', "2.10.0"
+  nuspec.dependency 'Newtonsoft.Json', "6.0.8"
+  nuspec.dependency 'RabbitMQ.Client', "3.4.3"
+  nuspec.output_file = File.join(props[:artifacts], 'RapidTransit.Integration.RabbitMQ.nuspec')
+  add_files props[:output], 'RapidTransit.Integration.RabbitMQ.{dll,pdb,xml}', nuspec
+  nuspec.file(File.join(props[:src], "RapidTransit.Integration.RabbitMQ\\**\\*.cs").gsub("/","\\"), "src")
+end
+
+nuspec :_nuspec do |nuspec|
   nuspec.id = 'RapidTransit.Integration.Web'
   nuspec.version = NUGET_VERSION
-  nuspec.authors = 'Chris Patterson'
+  nuspec.authors = ["Chris Patterson"]
   nuspec.description = 'RapidTransit, a quick library to get started building autonomous services using MassTransit'
   nuspec.title = 'RapidTransit.Integration.Web'
-  nuspec.projectUrl = 'http://github.com/MassTransit/RapidTransit'
+  nuspec.project_url = 'http://github.com/MassTransit/RapidTransit'
   nuspec.language = "en-US"
-  nuspec.licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0"
-  nuspec.requireLicenseAcceptance = "false"
+  nuspec.license_url = "http://www.apache.org/licenses/LICENSE-2.0"
   nuspec.dependency "RapidTransit.Core", NUGET_VERSION
   nuspec.dependency "RapidTransit.Integration", NUGET_VERSION
-  nuspec.dependency 'Autofac', "3.3.0"
-  nuspec.dependency 'Automatonymous', "1.2.5"
-  nuspec.dependency 'Automatonymous.MassTransit', "1.2.5"
-  nuspec.dependency 'Magnum', "2.1.2"
-  nuspec.dependency 'MassTransit', "2.9.5"
-  nuspec.dependency 'MassTransit.Autofac', "2.9.5"
-  nuspec.dependency 'MassTransit.Courier', "2.9.5"
-  nuspec.dependency 'MassTransit.RabbitMQ', "2.9.5"
-  nuspec.dependency 'MassTransit.Scheduling', "1.2.5"
-  nuspec.dependency 'Newtonsoft.Json', "5.0.8"
-  nuspec.dependency 'RabbitMQ.Client', "3.2.1"
+  nuspec.dependency 'Autofac', "3.5.2"
+  nuspec.dependency 'Automatonymous', "1.2.9"
+  nuspec.dependency 'Automatonymous.MassTransit', "1.2.9"
+  nuspec.dependency 'Magnum', "2.1.3"
+  nuspec.dependency 'MassTransit', "2.10.0"
+  nuspec.dependency 'MassTransit.Autofac', "2.10.0"
+  nuspec.dependency 'MassTransit.Courier', "2.10.0"
+  nuspec.dependency 'Newtonsoft.Json', "6.0.8"
   nuspec.dependency 'Taskell', "0.1.2"
   nuspec.output_file = File.join(props[:artifacts], 'RapidTransit.Integration.Web.nuspec')
   add_files props[:output], 'RapidTransit.Integration.Web.{dll,pdb,xml}', nuspec
@@ -191,35 +234,60 @@ end
 nuspec :_nuspec do |nuspec|
   nuspec.id = 'RapidTransit.Integration.Services'
   nuspec.version = NUGET_VERSION
-  nuspec.authors = 'Chris Patterson'
+  nuspec.authors = ["Chris Patterson"]
   nuspec.description = 'RapidTransit, a quick library to get started building autonomous services using MassTransit'
   nuspec.title = 'RapidTransit.Integration.Services'
-  nuspec.projectUrl = 'http://github.com/MassTransit/RapidTransit'
+  nuspec.project_url = 'http://github.com/MassTransit/RapidTransit'
   nuspec.language = "en-US"
-  nuspec.licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0"
-  nuspec.requireLicenseAcceptance = "false"
+  nuspec.license_url = "http://www.apache.org/licenses/LICENSE-2.0"
   nuspec.dependency "RapidTransit.Core", NUGET_VERSION
   nuspec.dependency "RapidTransit.Integration", NUGET_VERSION
-  nuspec.dependency 'Autofac', "3.3.0"
-  nuspec.dependency 'Automatonymous', "1.2.5"
-  nuspec.dependency 'Automatonymous.MassTransit', "1.2.5"
-  nuspec.dependency 'Common.Logging', '2.2.0'
-  nuspec.dependency 'Common.Logging.Core', '2.2.0'
-  nuspec.dependency 'Magnum', "2.1.2"
-  nuspec.dependency 'MassTransit', "2.9.5"
-  nuspec.dependency 'MassTransit.Autofac', "2.9.5"
-  nuspec.dependency 'MassTransit.Courier', "2.9.5"
+  nuspec.dependency 'Autofac', "3.5.2"
+  nuspec.dependency 'Automatonymous', "1.2.9"
+  nuspec.dependency 'Automatonymous.MassTransit', "1.2.9"
+  nuspec.dependency 'Common.Logging', '3.2.0'
+  nuspec.dependency 'Common.Logging.Core', '3.2.0'
+  nuspec.dependency 'Magnum', "2.1.3"
+  nuspec.dependency 'MassTransit', "2.10.0"
+  nuspec.dependency 'MassTransit.Autofac', "2.10.0"
+  nuspec.dependency 'MassTransit.Courier', "2.10.0"
   nuspec.dependency 'MassTransit.QuartzIntegration', '1.2.5'
-  nuspec.dependency 'MassTransit.RabbitMQ', "2.9.5"
+  nuspec.dependency 'MassTransit.RabbitMQ', "2.10.0"
   nuspec.dependency 'MassTransit.Scheduling', '1.2.5'
-  nuspec.dependency 'Newtonsoft.Json', "5.0.8"
-  nuspec.dependency 'Quartz', '2.2.2'
-  nuspec.dependency 'RabbitMQ.Client', "3.2.1"
+  nuspec.dependency 'Newtonsoft.Json', "6.0.8"
+  nuspec.dependency 'Quartz', '2.3.3'
   nuspec.dependency 'Taskell', "0.1.2"
-  nuspec.dependency 'Topshelf', '3.1.3'
+  nuspec.dependency 'Topshelf', '3.2.0'
   nuspec.output_file = File.join(props[:artifacts], 'RapidTransit.Integration.Services.nuspec')
   add_files props[:output], 'RapidTransit.Integration.Services.{dll,pdb,xml}', nuspec
   nuspec.file(File.join(props[:src], "RapidTransit.Integration.Services\\**\\*.cs").gsub("/","\\"), "src")
+end
+
+nuspec :_nuspec do |nuspec|
+  nuspec.id = 'RapidTransit.Integration.Services.Azure'
+  nuspec.version = NUGET_VERSION
+  nuspec.authors = ["Chris Patterson", "Peter Davis"]
+  nuspec.description = 'RapidTransit, a quick library to get started building autonomous services using MassTransit'
+  nuspec.title = 'RapidTransit.Integration.Services'
+  nuspec.project_url = 'http://github.com/MassTransit/RapidTransit'
+  nuspec.language = "en-US"
+  nuspec.license_url = "http://www.apache.org/licenses/LICENSE-2.0"
+  nuspec.dependency "RapidTransit.Core", NUGET_VERSION
+  nuspec.dependency "RapidTransit.Integration", NUGET_VERSION
+  nuspec.dependency "RapidTransit.Integration.Services", NUGET_VERSION
+  nuspec.dependency 'Autofac', "3.5.2"
+  nuspec.dependency 'Automatonymous', "1.2.9"
+  nuspec.dependency 'Magnum', "2.1.3"
+  nuspec.dependency 'MassTransit', "2.10.0"
+  nuspec.dependency 'MassTransit.Courier', "2.10.0"
+  nuspec.dependency 'Newtonsoft.Json', "6.0.8"
+  nuspec.dependency 'Taskell', "0.1.2"
+  nuspec.dependency 'Topshelf', '3.2.0'
+  nuspec.dependency 'Topshelf.Azure', '1.0.0'
+  nuspec.dependency 'Unofficial.Microsoft.WindowsAzure.ServiceRuntime', '2.5.0.0'
+  nuspec.output_file = File.join(props[:artifacts], 'RapidTransit.Integration.Services.Azure.nuspec')
+  add_files props[:output], 'RapidTransit.Integration.Services.Azure.{dll,pdb,xml}', nuspec
+  nuspec.file(File.join(props[:src], "RapidTransit.Integration.Services.Azure\\**\\*.cs").gsub("/","\\"), "src")
 end
 
 def project_outputs(props)
